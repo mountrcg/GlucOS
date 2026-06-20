bea145538fa2fcdf455da75d4b4ca07452c3dbce
Robert <comps@diprobe.com>
2026-06-20 09:43:39 +0200

    nightscout cgm

--- ios/BioKernel/BioKernel/LoopKitSupport/CGMManager.swift
+++ ios/BioKernel/BioKernel/LoopKitSupport/CGMManager.swift
@@ -11,23 +11,28 @@ import MockKit
 import G7SensorKit
 import G7SensorKitUI
 import CGMBLEKit
+import NightscoutRemoteCGM
 
 let g7ManagerIdentifier: String = "G7CGMManager"
 let g7LocalizedTitle = "Dexcom G7"
 let g6ManagerIdentifier: String = "DexG6Transmitter"
 let g6LocalizedTitle = "Dexcom G6"
+let nightscoutManagerIdentifier: String = NightscoutRemoteCGM.pluginIdentifier
+let nightscoutLocalizedTitle = "Nightscout Remote CGM"
 
 let staticCGMManagersByIdentifier: [String: CGMManager.Type] = [
     MockCGMManager.pluginIdentifier: MockCGMManager.self,
     g7ManagerIdentifier: G7CGMManager.self,
-    g6ManagerIdentifier: G6CGMManager.self
+    g6ManagerIdentifier: G6CGMManager.self,
+    nightscoutManagerIdentifier: NightscoutRemoteCGM.self
 ]
 
 var availableStaticCGMManagers: [CGMManagerDescriptor] {
     return [
             CGMManagerDescriptor(identifier: MockCGMManager.pluginIdentifier, localizedTitle: MockCGMManager.localizedTitle),
             CGMManagerDescriptor(identifier: g7ManagerIdentifier, localizedTitle: g7LocalizedTitle),
-            CGMManagerDescriptor(identifier: g6ManagerIdentifier, localizedTitle: g6LocalizedTitle)
+            CGMManagerDescriptor(identifier: g6ManagerIdentifier, localizedTitle: g6LocalizedTitle),
+            CGMManagerDescriptor(identifier: nightscoutManagerIdentifier, localizedTitle: nightscoutLocalizedTitle)
         ]
 }
 
